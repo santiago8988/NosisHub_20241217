c65b39a0125bdb98de091dffdda1c42e614d2891
Santiago Dominguez <73175059+santiago8988@users.noreply.github.com>
2024-12-17 13:11:03 -0300

    inicio/admin

--- app/inicio/layout.jsx
+++ app/inicio/layout.jsx
@@ -1,5 +1,78 @@
-
 import { ToastContainer } from "react-toastify"
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { getOrganizationAction } from "../_actions"
+import { FileText, Folder, LockKeyhole, Home, Calculator, Book } from 'lucide-react'
+import Sidebar2 from "../components/ui/SideBar2"
+
+const itemsAdmin = [
+  {
+    label: 'Admin',
+    href: '/inicio/admin',
+    icon: <LockKeyhole className="inline-block mr-2 h-5 w-5" />
+  },
+  {
+    label: 'Entradas',
+    href: '/inicio',
+    icon: <FileText className="inline-block mr-2 h-5 w-5" />
+  },
+  {
+    label: 'Documentos',
+    href: '/inicio/documentos',
+    icon: <Folder className="inline-block mr-2 h-5 w-5" />
+  },
+]
+
+const itemsUser = [
+  {
+    label: 'Entradas',
+    href: '/inicio',
+    icon: <FileText className="inline-block mr-2 h-5 w-5" />
+  },
+]
+
+const botitems = [
+  {
+    label: 'Formulas Playground',
+    href: '/formulas-playground',
+    icon: <Calculator className="inline-block mr-2 h-5 w-5" />
+  },
+  {
+    label: 'Documentación',
+    href: '/documentacion',
+    icon: <Book className="inline-block mr-2 h-5 w-5" />
+  },
+]
+
+export default async function InicioLayout({ children }) {
+  const session = await getServerSession(authOptions)
+  let organization = null
+
+  if (session?.user?.organization) {
+    organization = await getOrganizationAction(session.user.organization)
+  }
+
+  const sidebarItems = session?.user?.role === 'admin' ? itemsAdmin : itemsUser
+
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar2 
+        items={sidebarItems} 
+        botItems={botitems} 
+        name={session?.user?.name} 
+        organization={organization?.data?.name}
+      />
+      <main className="flex-1 ml-64 overflow-x-hidden overflow-y-auto">
+        {children}
+      </main>      
+      <ToastContainer/>
+    </div>
+  )
+}
+
+
+
+/*import { ToastContainer } from "react-toastify"
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../api/auth/[...nextauth]/route'
 import { getOrganizationAction } from "../_actions"
@@ -51,7 +124,7 @@ const botitems=[
 
 ]
 
-/*className="flex flex-1 overflow-hidden">*/ 
+
 const InicioLayout = async ({children}) => {
     const session = await getServerSession(authOptions)
     let organization = null
@@ -73,4 +146,4 @@ const InicioLayout = async ({children}) => {
   )
 }
 
-export default InicioLayout
+export default InicioLayout*/
